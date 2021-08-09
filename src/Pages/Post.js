19876a63e689045645ae2465ad7f8fa7ffd734c7
aumi364 <aumi.asif@gmail.com>
2021-08-09 16:00:12 +0600

    restructure context

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -1,60 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import BlogRouter from "../BlogRouter";
 import style from "./Post.module.css";
-import PostContext from "Utils/Context/PostContext.js";
-import { getPosts } from "Services/PostService";
-const Post = () => {
-  const [posts, setPosts] = useState([]); //initialized state for storing posts
-  const [toggle, setToggle] = useState(false); // to track if new data is inserted
-  const [error, setError] = useState(); // if any error returns
+import { PostContextProvider } from "Utils/Context/PostContext.js";
 
-  useEffect(() => {
-    let localToggler = localStorage.getItem("toggler");
-    let payload = localStorage.getItem("payload");
-    //check if both available in local storage
-    if (localToggler && payload) {
-      // check if user committed a post request
-      if (localToggler !== toggle.toString()) {
-        (async () => {
-          const response = await getPosts();
-          if (response.status === 200) {
-            localStorage.setItem("payload", JSON.stringify(response.data)); // storing in localstorage for preventing duplicate fetching
-            localStorage.setItem("toggler", toggle); // fetching if new data inserted
-            setPosts(response.data); //storing the response in the state
-          } else {
-            setError(response.status);
-          }
-        })();
-      } else {
-        const data = JSON.parse(payload);
-        setPosts(data);
-      }
-    } else {
-      localStorage.setItem("toggler", toggle);
-      (async () => {
-        const response = await getPosts();
-        if (response.status === 200) {
-          localStorage.setItem("payload", JSON.stringify(response.data));
-          setPosts(response.data); //storing the response in the state
-        } else {
-          setError(response.status);
-        }
-      })();
-    }
-  }, [toggle]);
+const Post = () => {
   return (
-    <PostContext.Provider
-      value={{
-        toggler: { toggle, setToggle },
-        viewError: { error, setError },
-        posts: posts,
-        error: error,
-      }}
-    >
+    <PostContextProvider>
       <div className={style.Post}>
         <BlogRouter />
       </div>
-    </PostContext.Provider>
+    </PostContextProvider>
   );
 };
 
--- src/Utils/Context/PostContext.js
+++ src/Utils/Context/PostContext.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getPosts } from "Services/PostService";
+const PostContext = React.createContext({
+  toggler: {},
+  posts: null,
+  error: null,
+});
 
-const PostContext = React.createContext(null);
-
+export const PostContextProvider = ({ children }) => {
+  const [posts, setPosts] = useState([]); //initialized state for storing posts
+  const [toggle, setToggle] = useState(false); // to track if new data is inserted
+  const [error, setError] = useState(); // if any error returns
+  useEffect(() => {
+    (async () => {
+      const response = await getPosts();
+      if (response.status === 200) {
+        setPosts(response.data); //storing the response in the state
+      } else {
+        setError(response.status);
+      }
+    })();
+  }, [toggle]);
+  return (
+    <PostContext.Provider
+      value={{
+        toggler: { toggle, setToggle },
+        posts: posts,
+        error: error,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+};
 export default PostContext;
